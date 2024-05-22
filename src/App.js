5387b1032e21bcb1aa5b5cfb4dadb36abbf6c969
moojaa <129938297+moojaa@users.noreply.github.com>
2024-05-22 23:25:00 +0900

    cummit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,63 @@ import TodoBoard from "./components/TodoBoard";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import api from "./utils/api"
+import { useEffect, useState } from "react";
 
 function App() {
+  const[todoList,setTodoList]= useState([])
+  const[todoValue,setTodoValue]=useState('')
+
+  const getTasks=async()=>{
+    const response =await api.get('/tasks')
+    console.log("rrr",response)
+    setTodoList(response.data.data)
+  }
+
+  const addTask=async()=>{
+    try{
+      const response = await api.post('/tasks',{task:todoValue,isComplete:false})
+      if(response.status ===200){
+        console.log("성공")
+        setTodoValue('')
+        getTasks()
+      }else{
+        throw new Error("task can not bd added")
+      }
+    }catch(err){
+      console.log("error",err)
+    }
+  }
+
+  const putTask=async(id)=>{
+    try{
+      const task = todoList.find((item)=>item._id===id)
+      const response = await api.put(`/tasks/${id}`,{
+        isComplete: !task.isComplete
+      })
+      if(response.status ===200){
+        getTasks()
+      }
+    }catch(err){
+      console.log("error",err)
+    }
+  }
+
+  const delTask= async(id)=>{
+    try{
+      const response = await api.delete(`tasks/${id}`)
+      if(response.status ===200){
+        getTasks()
+      }
+    }catch(err){
+      console.log("error",err)
+    }
+  }
+
+  useEffect(()=>{
+    getTasks()
+  },[])
+
   return (
     <Container>
       <Row className="add-item-row">
@@ -16,14 +71,16 @@ function App() {
             type="text"
             placeholder="할일을 입력하세요"
             className="input-box"
+            value={todoValue}
+            onChange={(event)=>setTodoValue(event.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
+          <button className="button-add" onClick={addTask}>추가</button>
         </Col>
       </Row>
 
-      <TodoBoard />
+      <TodoBoard todoList={todoList} putTask={putTask} delTask={delTask} />
     </Container>
   );
 }
--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,11 +1,12 @@
 import React from "react";
+import TodoItem from "./TodoItem"
 
-const TodoBoard = () => {
+const TodoBoard = ({todoList,putTask,delTask}) => {
   return (
     <div>
       <h2>Todo List</h2>
+      {todoList.length>0? todoList.map((item)=><TodoItem item={item} putTask={putTask} delTask={delTask}/>) : <h2>There is no Item to show</h2>}
       {/* <TodoItem/> will be here once we get the todoList */}
-      <h2>There is no Item to show</h2>
     </div>
   );
 };
