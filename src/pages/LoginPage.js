7ddd189b95c19e156b786e70631e2c08c5b03007
moojaa <129938297+moojaa@users.noreply.github.com>
2024-05-27 22:29:25 +0900

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -20,7 +20,7 @@ const LoginPage = () => {
       if (response.status === 200) {
         setUser(response.data.user)
         sessionStorage.setItem("token", response.data.token)
-        api.defaults.headers["authorization"] = "Bearer" + response.data.token
+        api.defaults.headers["authorization"] = "Bearer " + response.data.token
         setError("")
         navigate("/")
       }
